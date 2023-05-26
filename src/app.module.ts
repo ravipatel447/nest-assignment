693ca5c857abb81898bc0e9db789a0a1469ee744
ravipatelsimform <ravi.patel@simformsolutions.com>
2023-05-26 19:08:42 +0530

    friday commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,51 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { OrderModule } from './order/order.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RoleModule, PermissionModule, UserModule, ProductModule, CartModule, OrderModule],
+  imports: [
+    RoleModule,
+    PermissionModule,
+    UserModule,
+    ProductModule,
+    CartModule,
+    OrderModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: ['.env', '.developement.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory(configService: ConfigService) {
+        return {
+          type: 'mysql',
+          host: configService.get('db.host'),
+          port: configService.get('db.port'),
+          username: configService.get('db.username'),
+          password: configService.get('db.password'),
+          database: configService.get('db.database'),
+          entities: [],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'test',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
